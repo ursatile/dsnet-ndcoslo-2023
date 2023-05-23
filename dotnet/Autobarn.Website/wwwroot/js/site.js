ca76863de57f3b4a31cb3912a432f0a9629683ac
Dylan Beattoie <dylan@dylanbeattie.net>
2023-05-23 16:32:53 +0200

    Final workshop code from NDC Oslo 2023

--- dotnet/Autobarn.Website/wwwroot/js/site.js
+++ dotnet/Autobarn.Website/wwwroot/js/site.js
@@ -1,15 +1,28 @@
 
 function displayNotification(user, message) {
-	console.log(user);
-	console.log(message);
+	const target = document.getElementById('signalr-notifications');
+	const data = JSON.parse(message);
+	const div = document.createElement("DIV");
+	div.innerHTML = `<p>${data.Make} ${data.Model} (${data.Color}, ${data.Year})</p>
+<p>Price: ${data.Price} ${data.CurrencyCode}</p>
+<hr />
+<a href="/vehicles/details/${data.Registration}">Click for details...</a>`;
+	div.style.backgroundColor = data.Color;
+	target.append(div);
+	window.setTimeout(function () {
+		div.classList.add("hide");
+		window.setTimeout(function () {
+			div.parentNode.removeChild(div);
+		}, 1000);
+	}, 5000);
 }
 
 function connectSignalR() {
 	const conn = new signalR.HubConnectionBuilder().withUrl("/hub").build();
 	conn.on("DoTheCrazyClownThing", displayNotification);
-	conn.start().then(function() {
+	conn.start().then(function () {
 		console.log("SignalR is running!");
-	}).catch(function(err) {
+	}).catch(function (err) {
 		;
 		console.log("SignalR Failed");
 		console.error(err);
@@ -21,7 +34,7 @@ function ready(fn) {
 		fn();
 	} else {
 		document.addEventListener('DOMContentLoaded', fn);
-}				
+	}
 }
 
 ready(connectSignalR);
